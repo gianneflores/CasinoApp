86eda7dcf9a7dda8d09d9b6f39ab88f332e04159
Gianne Flores <gianneflores@giannes-macbook-pro.local>
2016-09-22 12:53:08 -0400

    Initial Commit

---
+++ Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project
+//
+//  Created by Gianne Flores on 9/22/16.
+//  Copyright © 2016 Gianne Flores. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
